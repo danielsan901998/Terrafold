e7dc0b6b140e45b22a45262b932090f055f34cdb
danielsan901998 <danielsan901998@gmail.com>
2025-12-31 14:41:25 +0100

    cache Tractor Beam Comet text

--- src/ui/TractorBeamView.ts
+++ src/ui/TractorBeamView.ts
@@ -3,6 +3,7 @@ import { intToString } from '../utils/utils';
 import BaseView from './BaseView';
 
 export default class TractorBeamView extends BaseView {
+    private cometRows: Map<number, HTMLElement> = new Map();
 
     constructor() {
         super();
@@ -29,21 +30,36 @@ export default class TractorBeamView extends BaseView {
     update() {
         if (!game || !view) return;
         const container = this.getElement("allPassing");
-        let text = "";
         const comets = game.tractorBeam.comets;
+
+        // Remove old comets that are no longer present
+        const cometIds = new Set(comets.map(c => c.id));
+        for (const [id, row] of this.cometRows.entries()) {
+            if (!cometIds.has(id)) {
+                row.remove();
+                this.cometRows.delete(id);
+            }
+        }
+
         for (let i = 0; i < comets.length; i++) {
             const comet = comets[i];
             if (!comet) continue;
-            text += comet.name + " with " +
-                intToString(comet.amount, 1) +
-                " " + comet.amountType +
-                " passing in " + comet.duration + "<br>";
+
+            if (!this.cometRows.has(comet.id)) {
+                const row = document.createElement("div");
+                row.className = "comet-row";
+                row.innerHTML = `<span>${comet.name}</span> with <span id="comet-amount-${comet.id}"></span> ${comet.amountType} passing in <span id="comet-duration-${comet.id}"></span>`;
+
+                container.appendChild(row);
+                this.cometRows.set(comet.id, row);
+            }
+
+            this.updateElementText(`comet-amount-${comet.id}`, intToString(comet.amount, 1));
+            this.updateElementText(`comet-duration-${comet.id}`, String(comet.duration));
+
             view.drawComet(comet);
         }
-        if (this.textCache.get("allPassing") !== text) {
-            container.innerHTML = text;
-            this.textCache.set("allPassing", text);
-        }
+
         this.updateElementText('takeAmount', String(game.tractorBeam.takeAmount));
     }
 }
